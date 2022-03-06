697e1289130cb1da7c4203754ad1ab3539ee9d51
Oguz <yilmazoguz@outlook.com>
2022-03-07 01:03:38 +0300

    Give color as a prop to Child function

--- src/Child.js
+++ src/Child.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
+function Child({ onChangeColor, color }) {
   return (
-    <div className="child" style={{ backgroundColor: "#FFF" }}
+    <div className="child" style={{ backgroundColor: color }}
     onClick={()=>onChangeColor(getRandomColor)}/>
   ) 
 }

913679d1eb175666de941cbce6b2e9bac626caea
Oguz <yilmazoguz@outlook.com>
2022-03-07 01:02:32 +0300

    Pass onChangeColor function to onClick event in Child.js

--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,10 @@ import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  return (
+    <div className="child" style={{ backgroundColor: "#FFF" }}
+    onClick={()=>onChangeColor(getRandomColor)}/>
+  ) 
 }
 
 export default Child;

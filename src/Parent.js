b4e2a3baa3058903b06236cb68e2996fbe6be860
Oguz <yilmazoguz@outlook.com>
2022-03-07 01:06:27 +0300

    Complete function of Parent with handle function and Child components

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,16 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  function handleChangeColor(newChildColor){
+    setColor(getRandomColor());
+    setChildrenColor(newChildColor);
+  }
+
   return (
+    
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={color} onChangeColor={handleChangeColor} />
+      <Child color={color} onChangeColor={handleChangeColor} />
     </div>
   );
 }
